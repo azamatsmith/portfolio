3cc56c64a48b1a92d89be6ee4f338eaec23bc543
Matthew Smith <azamatsmith@gmail.com>
2017-12-09 10:36:50 -0700

    Remove logging

--- src/actions/BooksActions.js
+++ src/actions/BooksActions.js
@@ -1,18 +1,17 @@
 const updateBooks = books => ({
-  type: 'UPDATE_BOOKS',
-  books,
+  type: "UPDATE_BOOKS",
+  books
 });
 
 export const getBooks = () => dispatch => {
   const url =
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3005/'
-      : 'https://tw-portfolio-mts.herokuapp.com/';
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3005/"
+      : "https://tw-portfolio-mts.herokuapp.com/";
   fetch(url)
     .then(res => res.json())
     .then(json => {
-      console.log('json is: ', json);
       return dispatch(updateBooks(json));
     })
-    .catch(err => console.log('error getting books: ', err));
+    .catch(err => console.log("error getting books: ", err));
 };
