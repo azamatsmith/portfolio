e5aca51ad097832e2365d9d2f9b463c287fce39d
Matthew Smith <azamatsmith@gmail.com>
2020-10-04 17:30:21 -0700

    Update packages and fix eslint errors

--- src/components/Admin/AddProject.js
+++ src/components/Admin/AddProject.js
@@ -1,39 +1,39 @@
 // AddProject.js
 
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {firebase, helpers} from 'react-redux-firebase';
-const {dataToJS} = helpers;
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { firebase, helpers } from 'react-redux-firebase';
 
 import Button from '../Shared/Button';
 import Input from '../Shared/Input';
 import TextArea from '../Shared/TextArea';
 
-class AddProject extends Component {
+const { dataToJS } = helpers;
 
+class AddProject extends Component {
   state = {
     title: '',
     org: '',
     summary: '',
     linkUrl: '',
     linkName: '',
-  }
+  };
 
   // PRIVATE
 
   _handleAdd = () => {
     this.props.firebase.push('/projects', this.state);
-    this.setState({ title: '', org: '', summary: '', linkUrl: '', linkName: '' });
-  }
+    this.setState({
+      title: '',
+      org: '',
+      summary: '',
+      linkUrl: '',
+      linkName: '',
+    });
+  };
 
   render() {
-    const {
-      title,
-      org,
-      summary,
-      linkUrl,
-      linkName
-    } = this.state;
+    const { title, org, summary, linkUrl, linkName } = this.state;
 
     return (
       <div className="AddProject admin-section">
@@ -43,52 +43,44 @@ class AddProject extends Component {
           placeholder="Title"
           type="text"
           value={title}
-          onChange={(e) => this.setState({title: e.target.value})}
+          onChange={e => this.setState({ title: e.target.value })}
         />
 
         <Input
           placeholder="Organization"
           type="text"
           value={org}
-          onChange={(e) => this.setState({org: e.target.value})}
+          onChange={e => this.setState({ org: e.target.value })}
         />
 
         <Input
           placeholder="Link Url"
           type="text"
           value={linkUrl}
-          onChange={(e) => this.setState({linkUrl: e.target.value})}
+          onChange={e => this.setState({ linkUrl: e.target.value })}
         />
 
         <Input
           placeholder="Link Name"
           type="text"
           value={linkName}
-          onChange={(e) => this.setState({linkName: e.target.value})}
+          onChange={e => this.setState({ linkName: e.target.value })}
         />
 
         <TextArea
           placeholder="Summary"
           type="text"
           value={summary}
-          onChange={(e) => this.setState({summary: e.target.value})}
+          onChange={e => this.setState({ summary: e.target.value })}
         />
 
-        <Button
-          onClick={ this._handleAdd }
-          text="Add Project"
-        />
-
+        <Button onClick={this._handleAdd} text="Add Project" />
       </div>
     );
   }
 }
 
-const data = firebase([
-  'projects'
-])(AddProject);
-export default connect(
-  ({ firebase }) => ({
-    projects: dataToJS(firebase, 'projects')
-  })
-)(data);
+const data = firebase(['projects'])(AddProject);
+export default connect(({ firebase }) => ({
+  projects: dataToJS(firebase, 'projects'),
+}))(data);
---
+++ src/components/ComingSoon/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ComingSoon() {
+  return (
+  )
+}
+
+ComingSoon.propTypes = {}
+
+ComingSoon.defaultProps = {}
+
+export default ComingSoon;
