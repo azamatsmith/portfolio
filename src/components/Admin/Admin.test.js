9d6c7ee943bc59a1f8a3ee86004c1f55ab5a8879
Matthew Smith <azamatsmith@users.noreply.github.com>
2020-10-20 06:25:50 -0700

    Config - Update Node version in Travis config (#17)
    * Update Travis config - increment node version
    
    * Update travis config
    
    * Add test

---
+++ src/components/Admin/Admin.test.js
@@ -0,0 +1,7 @@
+// import React from 'react';
+// import { render } from '@testing-library/react';
+// import Admin from './Admin';
+
+test('renders learn react link', () => {
+  expect(true).toBe(true);
+});
